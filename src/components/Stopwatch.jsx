cbc0c2e0bb78606262ffa3427ba9bb79754e15b3
Nathan Minchow <nathan.minchow@outlook.com>
2020-07-11 16:25:39 -0500

    Icons, better styling

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -58,14 +58,6 @@ class Stopwatch extends Component {
     this.setState({ displayColorPicker: false });
   };
 
-  handleTextAreaFocus = () => {
-    this.setState({ textAreaStyle: { backgroundColor: "white" } });
-  };
-
-  handleTextAreaBlur = () => {
-    this.setState({ textAreaStyle: { backgroundColor: this.state.color } });
-  };
-
   render() {
     const { timerTime } = this.state;
     let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
@@ -114,8 +106,24 @@ class Stopwatch extends Component {
           <div className="Stopwatch-display">
             {hours} : {minutes} : {seconds} : {centiseconds}
           </div>
-          <button onClick={() => this.props.deleteWatch(this.props.id)}>
-            Delete
+          <button className="close-button" onClick={() => this.props.deleteWatch(this.props.id)}>
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-x"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"
+              />
+            </svg>
           </button>
         </div>
 
@@ -137,9 +145,6 @@ class Stopwatch extends Component {
           <textarea
             rows="1"
             onKeyDown={this.handleKeyPress}
-            style={this.state.textAreaStyle}
-            onFocus={this.handleTextAreaFocus}
-            onBlur={this.handleTextAreaBlur}
             defaultValue="Stopwatch"
           ></textarea>
         </div>
