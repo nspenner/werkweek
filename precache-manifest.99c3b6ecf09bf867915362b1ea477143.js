47e9b5dae36d383ca680f9afcce65cbb7b1625e3
Nathan Minchow <nathan.minchow@outlook.com>
2020-11-07 00:10:26 -0600

    Updates

---
+++ precache-manifest.99c3b6ecf09bf867915362b1ea477143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9069dc45507801839fd1308b793ebfff",
+    "url": "/werkweek/index.html"
+  },
+  {
+    "revision": "62b1530451a9c7c84edd",
+    "url": "/werkweek/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2c93393c40cb425e9cb0",
+    "url": "/werkweek/static/css/main.0623ef3c.chunk.css"
+  },
+  {
+    "revision": "62b1530451a9c7c84edd",
+    "url": "/werkweek/static/js/2.69b4e953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/werkweek/static/js/2.69b4e953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c93393c40cb425e9cb0",
+    "url": "/werkweek/static/js/main.c9e9ef7f.chunk.js"
+  },
+  {
+    "revision": "eebd276a4a6852b0a8c4",
+    "url": "/werkweek/static/js/runtime-main.5da2ad4e.js"
+  }
+]);
