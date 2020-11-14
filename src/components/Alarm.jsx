9ff79b9453e56692a302b2cfc0954ae512845788
Nathan Minchow <nathan.minchow@outlook.com>
2020-11-14 10:24:06 -0600

    Create validated form inputs

--- src/components/Alarm.jsx
+++ src/components/Alarm.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { defaultColorPalette } from "../resources/palettes";
+import WidgetTitle from "./WidgetTitle";
+import ColorPicker from "./ColorPicker";
+
 import PropTypes from "prop-types";
 import Select from "react-select";
-
-import WidgetTitle from "./WidgetTitle";
 import { set } from "idb-keyval";
 
 const options = [
@@ -13,8 +15,10 @@ const options = [
 class Alarm extends React.Component {
   state = {
     title: "Alarm",
-    color: "#ea4440",
-    selectedPeriod: "am",
+    color: defaultColorPalette.blue.dark,
+    selectedOption: { value: "am", label: "AM" },
+    hour: "",
+    minute: "",
   };
 
   handleChange = (e) => {
@@ -23,13 +27,60 @@ class Alarm extends React.Component {
     });
   };
 
-  handleSelectChange = selectedOption => {
+  handleColorChange = (color) => {
+    this.setState(
+      {
+        color: color.hex,
+      },
+      () => {
+        set(this.props.id, this.state);
+      }
+    );
+  };
+
+  handleHourChange = (e) => {
+    const hour = e.target.value.replace(/\D/, "");
+    const hourValue = parseInt(hour);
+    if ((hourValue >= 0 && hourValue <= 12) || !hour) {
+      this.setState({ hour }, () => set(this.props.id, this.state));
+    }
+  };
+
+  handleMinuteChange = (e) => {
+    const minute = e.target.value.replace(/\D/, "");
+    const minuteValue = parseInt(minute);
+    if ((minuteValue >= 0 && minuteValue <= 60) || !minute) {
+      this.setState({ minute }, () => set(this.props.id, this.state));
+    }
+  };
+
+  handleSelectChange = (selectedOption) => {
     this.setState({ selectedOption });
     console.log(`Option selected:`, selectedOption);
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     const { selectedOption } = this.state;
+    const customStyles = {
+      container: (provided) => ({
+        ...provided,
+        height: "100%",
+        fontFamily: "Fira Sans",
+        fontSize: "2rem",
+      }),
+      control: (provided) => ({
+        ...provided,
+        height: "100%",
+      }),
+      input: (provided) => ({
+        ...provided,
+        height: "inherit",
+      }),
+    };
     return (
       <div className="flex-container--column widget-container border-curve">
         <WidgetTitle
@@ -39,12 +90,12 @@ class Alarm extends React.Component {
           onChange={this.handleChange}
         />
         <div className="p--sml">
-          <form action="" className="flow--med">
+          <form onSubmit={this.handleSubmit} className="flow--med">
             <div className="flex-container--centered countdown-display">
               <input
                 value={this.state.hour}
                 name="hour"
-                onChange={this.handleCountdownInputChange}
+                onChange={this.handleHourChange}
                 maxLength="2"
                 placeholder="HH"
                 aria-label="Hour"
@@ -54,20 +105,36 @@ class Alarm extends React.Component {
               <input
                 value={this.state.minute}
                 name="minute"
-                onChange={this.handleCountdownInputChange}
+                onChange={this.handleMinuteChange}
                 maxLength="2"
                 placeholder="MM"
                 aria-label="Minute"
                 className="countdown__input  p--sml"
               />
-              <Select
-                name="selectedOption"
-                value={selectedOption}
-                onChange={this.handleChange}
-                options={options}
-              />
+              <div className="select-container">
+                <Select
+                  name="selectedOption"
+                  value={selectedOption}
+                  onChange={this.handleSelectChange}
+                  options={options}
+                  styles={customStyles}
+                />
+              </div>
+            </div>
+            <div className="button-list">
+              <button type="submit">
+                <ion-icon name="play"></ion-icon>
+                <span>Start</span>
+              </button>
             </div>
           </form>
+          <div className="button-list">
+            <ColorPicker
+              color={this.state.color}
+              displayColorPicker={false}
+              onChange={this.handleColorChange}
+            />
+          </div>
         </div>
       </div>
     );
